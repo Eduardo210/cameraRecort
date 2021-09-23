71027ddac6113e33e23484f0641164044c431f8e
Eduardo210 <48279066+Eduardo210@users.noreply.github.com>
2021-09-23 17:45:21 -0500

    resuelto final

--- app/src/main/java/com/example/camera/SecondActivity.kt
+++ app/src/main/java/com/example/camera/SecondActivity.kt
@@ -22,6 +22,7 @@ import android.util.DisplayMetrics
 import androidx.core.view.marginLeft
 import androidx.core.view.marginStart
 import androidx.core.view.marginTop
+import kotlin.math.roundToInt
 
 
 class SecondActivity : AppCompatActivity() {
@@ -43,76 +44,63 @@ class SecondActivity : AppCompatActivity() {
     }
 
     fun Bitmap.rotate(degrees: Float, bmIm: Bitmap): Bitmap {
-//        val matrix = Matrix().apply { postRotate(degrees) }
-//        return Bitmap.createBitmap(this, 15, 15, width, height, matrix, true)
-
-        val matrix = Matrix()
-
-//        matrix.postRotate(degrees)
-
-//        val scaledBitmap = Bitmap.createScaledBitmap(bmIm, width, height, true)
-
-        val rotation =
-            bmIm
-//            Bitmap.createBitmap(
-//            scaledBitmap,
-//            0,
-//            0,
-//            scaledBitmap.width,
-//            scaledBitmap.height,
-//            matrix,
-//            true
-//        )
-        val bitmapHeight: Int = rotation.height
-        val bitmapWidth: Int = rotation.width
 
 
+        val viewCamera = intent.getIntegerArrayListExtra("DimensionsReference")
 
-//        val FACTOR_H = 78 //5%
-//        val FACTOR_W = 65
-//
-//        val FACTOR_HI = 11 //5%
-//        val FACTOR_WI = 17
+        val bitmapHeight: Int = bmIm.height
+        val bitmapWidth: Int = bmIm.width
 
-        val frame = intent.getIntegerArrayListExtra("DimensionsFrame")
-        val reference = intent.getIntegerArrayListExtra("DimensionsReference")
+        val FACTOR_H = 100 * viewCamera!![0] / ((2 * viewCamera[1]) + viewCamera[0])
+        val FACTOR_HI = FACTOR_H * viewCamera[1] / viewCamera[0]
+
+        if (bmIm.height < bmIm.width) {
+
+            val bitmapWidthResized: Int = FACTOR_H * bitmapWidth / 100
 
 
-        val heightOriginal = frame!![0]
-        val widthOriginal = frame[2]
-        val heightFrame = reference!![0]
-        val widthFrame = reference[2]
-        val leftFrame = reference[3]
-        val topFrame = reference[1]
-        val heightReal = rotation.height
-        val widthReal = rotation.width
-        val widthFinal = widthFrame * widthReal / widthOriginal
-        val heightFinal = heightFrame * heightReal / heightOriginal
-        val leftFinal = leftFrame * widthReal / widthOriginal
-        val topFinal = topFrame * heightReal / heightOriginal
-//        val FACTOR_H = 100 * viewCamera!![0]/((2 * viewCamera[1])  + viewCamera[0])
-//        //val FACTOR_H = 50
-//        val FACTOR_W = 100 * viewCamera!![2]/(2 * viewCamera[3]  + viewCamera[2])
-//
-//        val FACTOR_HI = 100 * viewCamera[1]/viewCamera[0]
-//        val FACTOR_WI = 100 * viewCamera[3]/viewCamera[2]
-////
-//        val bitmapHeightResized: Int = FACTOR_H * bitmapHeight / 100
-//
-//        val bitmapWidthResized: Int = FACTOR_W  * bitmapWidth / 100
-//
-//        val bitmapHeightResizedY: Int = FACTOR_HI * bitmapHeight /100
-//        val bitmapWidthResizedX: Int = FACTOR_WI  * bitmapWidth / 100
-//
-//
-//
-//        val bitmapWidthResized: Int = FACTOR_W  * bitmapWidth / 100
-//        val bitmapHeightResized: Int = FACTOR_H * bitmapHeight / 100
-//
-//        val bitmapWidthResizedX: Int = FACTOR_WI  * bitmapWidth / 100
-//        val bitmapHeightResizedY: Int = FACTOR_HI * bitmapHeight / 100
+            val bitmapHeightResized: Int = (bitmapWidthResized / 1.6).roundToInt()
 
-        return Bitmap.createBitmap(rotation,leftFinal, topFinal, widthFinal, heightFinal)
+
+            val bitmapWidthResizedX: Int = FACTOR_HI * bitmapWidth / 100
+            val bitmapHeightResizedY: Int = (bitmapHeight - bitmapHeightResized) / 2
+
+            val matrix = Matrix().apply { postRotate(degrees) }
+
+
+            return Bitmap.createBitmap(
+                bmIm,
+                bitmapWidthResizedX,
+                bitmapHeightResizedY,
+                bitmapWidthResized,
+                bitmapHeightResized,
+                matrix,
+                true
+            )
+
+
+        } else {
+
+            val bitmapHeightResized: Int = FACTOR_H * bitmapHeight / 100
+
+
+            val bitmapWidthResized: Int = (bitmapHeightResized / 1.6).roundToInt()
+
+
+            val bitmapHeightResizedY: Int = FACTOR_HI * bitmapHeight / 100
+            val bitmapWidthResizedX: Int = ((bitmapWidth - bitmapWidthResized) / 2).toInt()
+
+            return Bitmap.createBitmap(
+                bmIm,
+                bitmapWidthResizedX,
+                bitmapHeightResizedY,
+                bitmapWidthResized,
+                bitmapHeightResized
+            )
+
+
+        }
+
 
     }
 
